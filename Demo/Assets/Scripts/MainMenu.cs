5a8424b71c6f9928f132cd868e790475007e6051
dan3143 <vdguevara@uninorte.edu.co>
2020-05-22 15:36:59 -0500

    Added demo with bluetooth management

---
+++ Demo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
